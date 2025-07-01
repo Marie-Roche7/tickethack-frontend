6b70c288d400faa258302d8d9c45d4d3bf50bd3b
Marie <marie.roche32@orange.fr>
2025-07-01 18:14:41 +0200

    frontend script js

---
+++ script-main.js
@@ -0,0 +1,11 @@
+document.querySelector('#btn-cart').addEventListener('click',
+  function () {
+   document.location.href = "index-cart.html";
+ }
+);
+
+document.querySelector('#btn-booking').addEventListener('click',
+  function () {
+   document.location.href = "index-booking.html";
+ }
+);
